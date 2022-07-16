295a831dfdf11926d386dbf898288d5105476352
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-16 12:31:35 -0300

    Início

---
+++ SimpleCurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleCurrencyConverter
+{
+    public class CurrencyConverter
+    {
+        public static double Iof = 6.0 / 100;
+        public static double ConvertToDollar(double dollarQuote, double valueInDollar)
+        {
+            double valueInReal = dollarQuote * valueInDollar;
+
+            return valueInReal + (valueInReal * Iof);
+        }
+    }
+}
---
+++ SimpleCurrencyConverter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace SimpleCurrencyConverter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            double dollarQuote = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double valueInDollar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Valor a ser pago em reais = {CurrencyConverter.ConvertToDollar(dollarQuote, valueInDollar).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
